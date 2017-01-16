58e5da034f4334c2aa2d2f11358a1dad2af91c23
Roger Frybarger <rfrylovescalculus@gmail.com>
2017-01-15 22:37:01 -0700

    State of code at 2017-01-15 22:37:00. Imported from my life before git

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, frame: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -16,6 +16,8 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
+  
+  win.maximize();
 
   // Open the DevTools.
   win.webContents.openDevTools()
