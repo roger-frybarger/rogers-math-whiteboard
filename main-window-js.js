d8fe0ce8ac55929eac8837f715f4b836d80addb9
Roger Frybarger <rfrylovescalculus@gmail.com>
2017-01-17 20:44:01 -0700

    State of code at 2017-01-17 20:44:00. Imported from my life before git

--- main-window-js.js
+++ main-window-js.js
@@ -2,11 +2,17 @@
 const {ipcRenderer} = require('electron');
 const {dialog} = require('electron').remote;
 
+
+
+
 //window.addEventListener('touchstart', (evt) => console.log(evt.touches[0]));
-window.addEventListener('touchstart', blah);
-window.addEventListener('touchend', (evt) => console.log(evt.touches));
-window.addEventListener('mousedown', (evt) => console.log(evt));
-window.addEventListener('touchmove', (evt) => console.log(evt.touches));
+window.addEventListener('touchstart', tstart);
+window.addEventListener('touchend', tend);
+window.addEventListener('touchmove', tmove);
+window.addEventListener('touchcancel', tcancel);
+window.addEventListener('mousedown', mdown);
+window.addEventListener('mousemove', mmove);
+window.addEventListener('mouseup', mup);
 
 var safeToClose = false; // Eventually, this default will be true.
 
@@ -16,7 +22,7 @@ var safeToClose = false; // Eventually, this default will be true.
 ipcRenderer.on('close-button-clicked', () => {
   if(!safeToClose){
     ipcRenderer.send('maximize-main-win');
-    var ret = dialog.showMessageBox({ type: 'warning', message: 'Warning: If you proceed, any\nchanges made to this set of\nimages will be lost.', buttons: ['Lose Changes', 'Cancel'], defaultId: 1 });
+    var ret = dialog.showMessageBox({ title: 'Warning:', type: 'warning', message: 'Warning: If you proceed, any\nchanges made to this set of\nimages will be lost.', buttons: ['Lose Changes', 'Cancel'], defaultId: 1 });
       
     if(ret == 0){
       ipcRenderer.send('terminate-this-app');
@@ -26,8 +32,44 @@ ipcRenderer.on('close-button-clicked', () => {
 });
 
 
-function blah(event){
+function tstart(event){
   
-  console.log(event);
+  console.log('touchstart');
+  
+}
+
+function tend(event){
+  
+  console.log('touchend');
+  
+}
+
+function tmove(event){
+  
+  console.log('touchmove');
+  
+}
+
+function tcancel(event){
+  
+  console.log('touchcancel');
+  
+}
+
+function mdown(event){
+  
+  console.log('mousedown');
+  
+}
+
+function mmove(event){
+  
+  console.log('mousemove');
+  
+}
+
+function mup(event){
+  
+  console.log('mouseup');
   
 }
